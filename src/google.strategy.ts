a41822eb896478d777304e9679ee565e20539557
Agrim-Ani <82039688+Agrim-Ani@users.noreply.github.com>
2024-03-13 12:59:11 +0530

    latest

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@nestjs/common';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: '550757291627-u953pjg0gs9m8sdsnf6oalipr9ivp7m8.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientID: '',
+      clientSecret: '',
       callbackURL: 'http://localhost:3000/auth/api/v1/google/callback',
       scope: ['email', 'profile'],
     });
